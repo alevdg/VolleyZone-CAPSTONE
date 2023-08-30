bdca19f1c601490627efa8172cd15dc9b3ecd525
alevdg <alessandravanessadg@gmail.com>
2023-08-30 17:00:25 +0200

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
