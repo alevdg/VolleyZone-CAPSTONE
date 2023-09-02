3da467f039dd7992d2b9bef105ef3dc5f7d007d2
alevdg <alessandravanessadg@gmail.com>
2023-09-02 17:33:27 +0200

    clean

--- src/app/component/log-register/log-register.component.ts
+++ src/app/component/log-register/log-register.component.ts
@@ -12,24 +12,8 @@ export class LogRegisterComponent {
   name: string = '';
   surname: string = '';
   role: string = '';
-  showLoginOverlay: boolean = false;
-  showRegisterOverlay: boolean = false;
 
   constructor(private authService: AuthService) { }
-
-  ngOnInit() {
-    const signUpButton = document.getElementById('signUp') as HTMLElement;
-    const signInButton = document.getElementById('signIn') as HTMLElement;
-    const container = document.getElementById('container') as HTMLElement;
-
-    signUpButton.addEventListener('click', () =>
-      container.classList.add('right-panel-active')
-    );
-
-    signInButton.addEventListener('click', () =>
-      container.classList.remove('right-panel-active')
-    );
-  }
 
   login() {
     this.authService.login(this.email, this.password);
