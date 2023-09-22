aa7dcadf623bdcace2cef18cdf1d461a7414c189
alevdg <alessandravanessadg@gmail.com>
2023-09-22 15:39:56 +0200

    Debug 1.1

--- src/app/pages/team-page/team-page.component.ts
+++ src/app/pages/team-page/team-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { iTeam } from './../../shared/teams';
+import { iUser } from './../../shared/user';
+import { combineLatest, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-team',
@@ -17,7 +20,21 @@ export class TeamPageComponent implements OnInit {
   }
 
   getTeamMembers(): void {
-    this.firestore.collection<iTeam>('Ftn8NLM9EE65oPlzMTMd').valueChanges().subscribe((teamMembers: iTeam[]) => {
+    this.firestore.doc<iTeam>('teams/Ftn8NLM9EE65oPlzMTMd').valueChanges().pipe(
+      switchMap((team: iTeam | undefined) => {
+        if (team) {
+          // Fetch each team member's data from the 'users' collection
+          const memberObservables = team.members.map(memberId =>
+            this.firestore.doc<iUser>(`users/${memberId}`).valueChanges()
+          );
+
+          // Combine the Observables into one Observable that emits an array of user data
+          return combineLatest(memberObservables);
+        } else {
+          return of([]);
+        }
+      })
+    ).subscribe((teamMembers: iTeam[]) => {
       this.teamMembers = teamMembers;
     });
   }
